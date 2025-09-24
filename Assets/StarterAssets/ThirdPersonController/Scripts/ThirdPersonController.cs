0bf3ed81b21c68d35b2a9c50676578b54fc46bc2
Navya Nandini <navyanandini2222@gmail.com>
2025-09-25 03:17:23 +0530

    bridge and railing prefabs set , camera clipping fixed

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -73,7 +73,7 @@ namespace StarterAssets
         public float CameraAngleOverride = 0.0f;
 
         [Tooltip("For locking the camera position on all axis")]
-        public bool LockCameraPosition = false;
+        public bool LockCameraPosition = true;
 
         // cinemachine
         public float _cinemachineTargetYaw;
