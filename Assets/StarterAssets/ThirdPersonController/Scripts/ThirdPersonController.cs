dbc89058a5228715f1f3f0d7dd6fbf0b80627b44
Navya Nandini <navyanandini2222@gmail.com>
2025-09-26 13:10:53 +0530

    uptodate

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -73,7 +73,7 @@ namespace StarterAssets
         public float CameraAngleOverride = 0.0f;
 
         [Tooltip("For locking the camera position on all axis")]
-        public bool LockCameraPosition = true;
+        public bool LockCameraPosition = false;
 
         // cinemachine
         public float _cinemachineTargetYaw;
